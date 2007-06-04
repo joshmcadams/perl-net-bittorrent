d4786357e2588f5604710a724064588d73a20859
joshua.mcadams <joshua.mcadams@27c31aed-0232-0410-ab44-3da39cfb857b>
2007-06-04 01:26:31 +0000

    tests refactored, along with a little code refactoring... we are now writing each piece to a different file and then combining those files after everything is said and done

--- trunk/lib/Net/BitTorrent/File/Downloaded.pm
+++ trunk/lib/Net/BitTorrent/File/Downloaded.pm
@@ -5,6 +5,8 @@ use strict;
 use Carp;
 use IO::File;
 use List::MoreUtils qw(first_index);
+use File::Slurp qw(read_file);
+use Digest::SHA1 qw(sha1);
 
 sub new {
     my ( $class, %args ) = @_;
@@ -24,7 +26,10 @@ sub _check_arguments {
     $self->{files}            = $self->{dt_obj}->get_files();
     $self->{remaining_pieces} =
       [ 0 .. $self->{dt_obj}->get_total_piece_count() - 1 ];
-    $self->{completed_pieces} = [];
+    $self->{completed_pieces}      = [];
+    $self->{pieces_sha1_hashes}    = $self->{dt_obj}->get_pieces_array();
+    $self->{standard_piece_length} =
+      $self->{dt_obj}->get_standard_piece_length();
 
     return $self;
 }
@@ -37,28 +42,69 @@ sub get_completed_piece_list {
     return $_[0]->{completed_pieces};
 }
 
-sub get_remaining_segments_list_for_piece {
+sub get_remaining_blocks_list_for_piece {
     my ( $self, $piece ) = @_;
-    return [ { offset => 0, size => 6169 } ];
+
+    if ( ( first_index { $piece == $_ } @{ $self->{completed_pieces} } ) >= 0 )
+    {
+        return [];
+    }
+
+    if ( -s $self->{files}->[0]->{path} . '.' . $piece ) {
+        return [ { offset => 9, size => 1 } ];
+    }
+
+    return [ { offset => 0, size => 10 } ];
 }
 
-sub write_segment {
+sub write_block {
     my ( $self, %args ) = @_;
 
     my $piece_index =
       first_index { $_ == $args{piece} } @{ $self->{remaining_pieces} };
     return unless $piece_index >= 0;
 
-    my $fh = IO::File->new( $self->{files}->[0]->{path}, 'w' ) || croak($!);
+    my $piece_file = $self->{files}->[0]->{path} . '.' . $piece_index;
+
+    my $fh = IO::File->new( $piece_file, 'a' ) || croak($!);
+    binmode $fh;
+    $fh->seek( $args{offset}, 0 );
     print {$fh} ${ $args{data_ref} } || croak($!);
     $fh->close();
 
-    if ( $self->{remaining_pieces}->[$piece_index] == $args{piece} ) {
+    if (
+        ( -s $piece_file == $self->{files}->[0]->{length} )
+        && ( $self->{pieces_sha1_hashes}->[$piece_index] eq
+            sha1( read_file( $piece_file, binmode => ':raw' ) ) )
+        && $self->{remaining_pieces}->[$piece_index] == $args{piece}
+      )
+    {
         push @{ $self->{completed_pieces} },
           splice @{ $self->{remaining_pieces} }, $piece_index, 1;
+
+        $self->_are_we_done_yet();
     }
 
     return 1;
+}
+
+sub _are_we_done_yet {
+    my ($self) = @_;
+
+    return if @{ $self->{remaining_pieces} };
+
+    my $final_file = $self->{files}->[0]->{path};
+
+    my $fh = IO::File->new( $final_file, 'w' ) || croak $!;
+    binmode($fh);
+
+    for my $piece ( sort { $a <=> $b } @{ $self->{completed_pieces} } ) {
+        my $piece_file = $self->{files}->[0]->{path} . '.' . $piece;
+        print {$fh} read_file( $piece_file, binmode => ':raw' ) || croak $!;
+        unlink $piece_file;
+    }
+
+    $fh->close();
 }
 
 1;
