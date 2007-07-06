e67d9b474702d12abccbe49fd765f5027beb1beb
joshua.mcadams <joshua.mcadams@27c31aed-0232-0410-ab44-3da39cfb857b>
2007-07-06 05:26:13 +0000

    r780@joshua-mcadams-powerbook-g4-12:  joshua | 2007-07-05 23:16:51 -0500  have file writing pretty much working, except for multiple files all getting globbed... about to fix that

--- trunk/trunk/lib/Net/BitTorrent/File/Downloaded.pm
+++ trunk/trunk/lib/Net/BitTorrent/File/Downloaded.pm
@@ -13,7 +13,7 @@ sub new {
     my ( $class, %args ) = @_;
     my $self = bless \%args, $class;
 
-    $self->_check_arguments();
+    $self->_check_arguments()->_pick_up_where_we_left_off();
 
     return $self;
 }
@@ -35,16 +35,30 @@ sub _check_arguments {
       $self->{dt_obj}->get_final_partial_piece_length();
     $self->{total_piece_count} = $self->{dt_obj}->get_total_piece_count();
 
-    #    for my $file (map { $_->{path} } @{$self->{files}}) {
-    #        my @path = File::Spec->splitdir($file);
-    #        pop @path;
-    #        for my $index (0..$#path) {
-    #            my $path = File::Spec->join(@path[0..$index]);
-    #            if(not -d $path) {
-    #                mkdir $path || die $!;
-    #            }
-    #        }
-    #    }
+    return $self;
+}
+
+sub _pick_up_where_we_left_off {
+    my ($self) = @_;
+
+    for my $piece_file ( glob('*.piece') ) {
+        my ($piece_number) = $piece_file;
+        $piece_number =~ s/\D//g;
+
+        if ( $self->{pieces_sha1_hashes}->[$piece_number] eq
+            sha1( read_file( $piece_file, binmode => ':raw' ) ) )
+        {
+            my $index_in_remaining =
+              first_index { $_ == $piece_number }
+              @{ $self->{remaining_pieces} };
+            push @{ $self->{completed_pieces} },
+              splice @{ $self->{remaining_pieces} }, $index_in_remaining, 1;
+            $self->_are_we_done_yet($piece_number);
+        }
+        else {
+            unlink $piece_file;
+        }
+    }
 
     return $self;
 }
@@ -66,9 +80,10 @@ sub get_remaining_blocks_list_for_piece {
     }
 
     my $piece_length =
-      ( $piece != ( $self->{total_piece_count} - 1 ) )
-      ? $self->{standard_piece_length}
-      : $self->{final_partial_piece_length};
+      ( $self->{final_partial_piece_length}
+          && ( $piece == ( $self->{total_piece_count} - 1 ) ) )
+      ? $self->{final_partial_piece_length}
+      : $self->{standard_piece_length};
 
     if ( my $size = -s $piece . '.piece' ) {
         return [ { offset => $size, size => $piece_length - $size } ];
@@ -84,7 +99,7 @@ sub write_block {
       first_index { $_ == $args{piece} } @{ $self->{remaining_pieces} };
     return unless $piece_index >= 0;
 
-    my $piece_file = $piece_index . '.piece';
+    my $piece_file = $args{piece} . '.piece';
 
     # write the data to a 'piece' file
     {
@@ -102,9 +117,8 @@ sub write_block {
 
     if (
         ( -s $piece_file == $completed_piece_length )
-        && ( $self->{pieces_sha1_hashes}->[$piece_index] eq
+        && ( $self->{pieces_sha1_hashes}->[ $args{piece} ] eq
             sha1( read_file( $piece_file, binmode => ':raw' ) ) )
-        && $self->{remaining_pieces}->[$piece_index] == $args{piece}
       )
     {
         push @{ $self->{completed_pieces} },
@@ -121,7 +135,7 @@ sub _are_we_done_yet {
 
     return if @{ $self->{remaining_pieces} };
 
-    my $final_file = $self->{files}->[$piece_index]->{path};
+    my $final_file = $self->{files}->[0]->{path};
 
     my @dirs = File::Spec->splitdir($final_file);
     pop @dirs;
